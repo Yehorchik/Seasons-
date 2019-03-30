7d2e0648ead68c8073dae9a8ab557071e74b8022
Yehor Melnyk <egorchikmelnyk@gmail.com>
2019-03-29 22:45:05 -0700

    Add Lifecycle methods + update season component

--- src/index.js
+++ src/index.js
@@ -3,29 +3,20 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = { latitude : null , errorMessage : ""};
+    state = { latitude : null , errorMessage : ""};
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) =>{
-                this.setState({latitude : position.coords.latitude});
-            },
-            (err) => {
-                this.setState({errorMessage : err.message});
-            }
+            position => this.setState({latitude : position.coords.latitude}),
+            err => this.setState({errorMessage : err.message})
         );
     }
     render(){
         if(this.state.errorMessage && !this.state.latitude){
-           return ( 
-             <div>Eror: {this.state.errorMessage}</div>
-           );
+           return <div>Eror: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.latitude){
-            return ( 
-              <div>Latitude: {this.state.latitude}</div>
-            );
+            return <SeasonDisplay lat = {this.state.latitude} />
          }
         else{
             return <div>Loading</div>
